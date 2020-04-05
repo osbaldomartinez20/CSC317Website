fa5ad8bbe6fb4a286baae727b740b367bd4e8c29
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2020-04-04 20:57:06 -0700

    added files from previous two assigments to project. Still working on making them work with their routes.

--- application/app.js
+++ application/app.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var path = require('path');
+var express      = require('express');
+var path         = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger       = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter     = require('./routes/index.js');
+var loginRouter     = require('./routes/login.js');
+var postimageRouter = require('./routes/postimage.js');
+var registerRouter  = require('./routes/register.js');
+
 
 var app = express();
 
@@ -15,6 +18,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/postimage', postimageRouter);
+app.use('/register', registerRouter);
 
 module.exports = app;
