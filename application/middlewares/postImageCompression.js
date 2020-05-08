59461c272431a3e95760ec34f22b78796ba0b93c
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2020-05-08 01:40:24 -0700

    removed gifs from image options.

--- application/middlewares/postImageCompression.js
+++ application/middlewares/postImageCompression.js
@@ -6,14 +6,13 @@ var compress_images = require('compress-images');
 var INPUT_path, OUTPUT_path;
 
 const imageCompression = (req, res, next) => {
-    INPUT_path = './public/images/uploads/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+    INPUT_path = './public/images/uploads/**/*.{jpg,JPG,jpeg,JPEG,png,svg}';
     OUTPUT_path = './public/images/uploads_compressed/';
 
     compress_images(INPUT_path, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
         { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
         { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
-        { svg: { engine: 'svgo', command: '--multipass' } },
-        { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } }, function (error, completed, statistic) {
+        { svg: { engine: 'svgo', command: '--multipass' } }, function (error, completed, statistic) {
             console.log('-------------');
             console.log(error);
             console.log(completed);
