0bd8bc85a2b1c7dfdd79a4895b6ca54d2013030f
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2020-05-07 23:43:44 -0700

    Image preview for posts works.

--- application/routes/search.js
+++ application/routes/search.js
@@ -7,24 +7,4 @@ const searchController = require('../controllers/searchController');
 // POST request for search
 router.post('/', searchController.post);
 
-// GET request to paginate and show search results
-router.get('/', searchController.get);
-
 module.exports = router;
-
-
-const db = require('../config/db');
-
-// Handle search redirection on POST
-exports.post = (req, res, next) => {
-    let keyword = req.body.keyword;
-    let sql = "SELECT * FROM posts";
-    if (keyword != "") {
-        sql += " WHERE "
-    }
-}
-
-// Handle rendering of search results on GET
-exports.get = (req, res, next) => {
-    console.log(req.body);
-}
